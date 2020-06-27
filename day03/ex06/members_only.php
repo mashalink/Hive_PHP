d53629b4d1656a6b3a4df3644df6c54f64c85c9a
Maria Link <maria8link@gmail.com>
2020-06-27 12:25:25 +0300

    finally

--- day03/ex06/members_only.php
+++ day03/ex06/members_only.php
@@ -1,10 +1,9 @@
 <?php
-
-	if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != 'zaz' || !isset($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_PW'] != 'Ilovemylittleponey') {
+	if ($_SERVER['PHP_AUTH_USER'] != 'zaz' || $_SERVER['PHP_AUTH_PW'] != 'Ilovemylittleponey') {
 		header('WWW-Authenticate: Basic realm=\'\'Member area\'\'');
 		header('HTTP/1.0 401 Unauthorized');
 		echo "<html><body>That area is accessible for members only";
-		echo "</body></html>\n";
+		echo "</body></html>    \n";
 	} else {
 		$pic = base64_encode(file_get_contents("../img/42.png"));
 		echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,";
