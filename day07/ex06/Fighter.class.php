4b1fa1ac989e553684b3963a360dacc9a99c6853
Maria Link <maria8link@gmail.com>
2020-07-06 12:20:35 +0300

    day07

---
+++ day07/ex06/Fighter.class.php
@@ -0,0 +1,15 @@
+<?php
+	class Fighter
+    {
+        private $name;
+
+        public function __construct($name)
+        {
+            $this->name = $name;
+        }
+
+        public function getName()
+        {
+            return $this->name;
+        }
+    }
